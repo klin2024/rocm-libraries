e6d34d441af6a0fb995a309ffb169301bd5edcf3
Adam Dickin <adam.dickin@amd.com>
2026-02-12 23:05:49 -0700

    [HipDNN]: Fix test timeouts on windows/linux due to exessive unloads/loads of miopen plugin (#4566)
    ## Motivation
    
    There have been test timeouts on windows and linux for the
    miopen-plugin. We need to fix that...
    
    ## Technical Details
    
    Looks like with our logging changes to remove spdlog, we now properly
    unload/load the miopen plugin. This means we initialize miopen multiple
    times now instead of once. This behaviour is more correct but causes an
    issue with the tests due to the unloads/loads. A quick fix for now is to
    hold a handle open in the integration tests so that the plugin doesnt
    unload after being loaded once restoring the old test behaviour. We will
    figure out a better fix in the future, I hope this fix is temporary.
    
    ## Test Plan
    
    CI tests dont timeout...
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- dnn-providers/miopen-provider/integration_tests/main.cpp
+++ dnn-providers/miopen-provider/integration_tests/main.cpp
@@ -8,15 +8,32 @@ SPDX-License-Identifier: MIT
 #include <hipdnn_frontend.hpp>
 #include <hipdnn_test_sdk/utilities/HipErrorHandler.hpp>
 
+#include <hipdnn_data_sdk/utilities/PlatformUtils.hpp>
+
+#include <hipdnn_backend.h>
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
 
     hipdnn_frontend::initializeFrontendLogging();
 
+    auto pluginPath = std::filesystem::weakly_canonical(
+        hipdnn_data_sdk::utilities::getCurrentExecutableDirectory() / PLUGIN_PATH);
+    const std::string pluginPathStr = pluginPath.string();
+    const std::array<const char*, 1> paths = {pluginPathStr.c_str()};
+
+    hipdnnSetEnginePluginPaths_ext(paths.size(), paths.data(), HIPDNN_PLUGIN_LOADING_ABSOLUTE);
+
+    hipdnnHandle_t handle;
+    hipdnnCreate(&handle);
+
     // Register HipErrorHandler to check and clear HIP errors after each test
     testing::TestEventListeners& listeners = testing::UnitTest::GetInstance()->listeners();
     listeners.Append(new hipdnn_test_sdk::utilities::HipErrorHandler);
 
-    return RUN_ALL_TESTS();
+    auto result = RUN_ALL_TESTS();
+
+    hipdnnDestroy(handle);
+    return result;
 }
